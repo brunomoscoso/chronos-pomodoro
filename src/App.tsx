eaaec7f17f4c787a962e31f444fcb0fb6bf6462e
Bruno Moscoso <brunomoscosorodrigues@gmail.com>
2025-05-16 12:41:09 +0100

    Adicionado novas funções

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 //A funcão tem que ter o nome em PascalCase
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
 
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
 export function App() {
-  console.log('Oi');
   return (
     <>
-      <Heading/>
-      <p>
-        Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Placeat quod omnis
-        deserunt pariatur voluptatum, maxime odio
-        molestias deleniti. Veniam recusandae,
-        natus modi deserunt eos sunt aperiam cum
-        reiciendis! Vel, ea.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
